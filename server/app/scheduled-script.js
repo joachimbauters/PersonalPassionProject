feef4af57be80bb7433d1022a70f9c8f3bb2502d
joachimbauters <joachim.bauters@student.howest.be>
2019-12-16 21:08:27 +0100

    socket weggedaan

--- server/app/scheduled-script.js
+++ server/app/scheduled-script.js
@@ -1,72 +1,69 @@
-const path = require("path");
-// const Abbonement = require(`${__dirname}/models/abbonement.model.js`);
-// const mongoose = require("mongoose");
+const Abbonement = require(`./server/app/models/abbonement.model.js`);
+const mongoose = require("mongoose");
 
-// require("dotenv").config();
+require("dotenv").config();
 
-// //const socket = require("socket.io-client")(process.env.SOCKET_URL);
+//const socket = require("socket.io-client")(process.env.SOCKET_URL);
 
-// mongoose
-//   .connect(process.env.DB_URL, {
-//     useNewUrlParser: true,
-//     useFindAndModify: false,
-//     useCreateIndex: true
-//   })
-//   .then(() => console.log("db connected"))
-//   .catch(e => {
-//     console.log("Error, exiting", e);
-//     process.exit();
-//   });
+mongoose
+  .connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  })
+  .then(() => console.log("db connected"))
+  .catch(e => {
+    console.log("Error, exiting", e);
+    process.exit();
+  });
 
 const checkAbbonement = async () => {
-  console.log("hey");
+  try {
+    const abbonementen = Abbonement.find(
+      { active: true },
+      "_id naam startTime endTime"
+    );
+    const abbonementenArray = await abbonementen.exec();
+    const now = new Date().toISOString();
+    abbonementenArray.map(async abbonement => {
+      try {
+        const d1 = Date.parse(now);
+        const d2 = Date.parse(abbonement.endTime);
+        if (d2 < d1) {
+          console.log("abbonement verlengd");
+          const startTimeNow = new Date().toISOString();
+          const endTime = new Date(startTimeNow);
+          endTime.setMonth(endTime.getMonth() + 1);
 
-  // try {
-  //   const abbonementen = Abbonement.find(
-  //     { active: true },
-  //     "_id naam startTime endTime"
-  //   );
-  //   const abbonementenArray = await abbonementen.exec();
-  //   const now = new Date().toISOString();
-  //   abbonementenArray.map(async abbonement => {
-  //     try {
-  //       const d1 = Date.parse(now);
-  //       const d2 = Date.parse(abbonement.endTime);
-  //       if (d2 < d1) {
-  //         console.log("abbonement verlengd");
-  //         const startTimeNow = new Date().toISOString();
-  //         const endTime = new Date(startTimeNow);
-  //         endTime.setMonth(endTime.getMonth() + 1);
+          await Abbonement.findByIdAndUpdate(
+            {
+              _id: abbonement._id
+            },
+            {
+              startTime: startTimeNow,
+              endTime: endTime
+            },
+            {
+              new: true
+            }
+          );
 
-  //         await Abbonement.findByIdAndUpdate(
-  //           {
-  //             _id: abbonement._id
-  //           },
-  //           {
-  //             startTime: startTimeNow,
-  //             endTime: endTime
-  //           },
-  //           {
-  //             new: true
-  //           }
-  //         );
+          // socket.emit(`notification`, {
+          //   notification: `${abbonement.naam} is hernieuwd voor een maand`
+          // });
 
-  //         // socket.emit(`notification`, {
-  //         //   notification: `${abbonement.naam} is hernieuwd voor een maand`
-  //         // });
-
-  //         //socket.disconnect();
-  //         mongoose.connection.close();
-  //       } else {
-  //         return null;
-  //       }
-  //     } catch (err) {
-  //       throw err;
-  //     }
-  //   });
-  // } catch (err) {
-  //   throw err;
-  // }
+          //socket.disconnect();
+          mongoose.connection.close();
+        } else {
+          return null;
+        }
+      } catch (err) {
+        throw err;
+      }
+    });
+  } catch (err) {
+    throw err;
+  }
 };
 
 checkAbbonement();
