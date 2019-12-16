ff3761d3437ddfd194332fd22bf8c567e914911e
joachimbauters <joachim.bauters@student.howest.be>
2019-12-16 18:02:28 +0100

    finale versie

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { GraphQLServer, PubSub } = require("graphql-yoga");
+const path = require("path");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
