adff549cbecb7cec3e0366d33a7280a52d547a20
joachimbauters <joachim.bauters@student.howest.be>
2019-12-16 18:12:47 +0100

    finale versie

--- server/socketApi.js
+++ server/socketApi.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const server = app.listen(process.env.PORT || 5000);
+const server = app.listen(5000);
 const io = require("socket.io")(server);
 
 exports.socketio_api = () => {
